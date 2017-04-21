ee4641ab649eb9a54d100db0fd2a2ca162b998cc
wf19920502 <zjuwf2010@163.com>
2017-04-21 14:05:26 +0800

    Signed-off-by: wf19920502 <zjuwf2010@163.com>

--- controllers/doctor_controller.js
+++ controllers/doctor_controller.js
@@ -358,7 +358,7 @@ exports.editDoctorDetail = function(req, res) {
 		upObj['city'] = req.body.city;
 	}
 	if (req.body.district != null){
-		doctorData['district'] = req.body.district;
+		upObj['district'] = req.body.district;
 	}
 	if (req.body.workUnit != null){
 		upObj['workUnit'] = req.body.workUnit;
